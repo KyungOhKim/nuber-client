c8e07b60a792d80052fbad9b9ea98881bef717fa
kko0831 <kko0831@hanmail.net>
2020-12-03 17:04:30 +0900

    cors anywhere customized

--- src/lib/mapHelpers.ts
+++ src/lib/mapHelpers.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const getCode = async (address: string) => {
   // 자체 서버를 사용하는 대신 클라이언트가 HTTP 요청을 하는 프록시 서버 링크를 URL에 추가하여 CORS 에러 해결
-  const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  const URL = `${process.env.REACT_APP_CORS_PROXY}/https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
   const { data } = await axios(URL);
   if (data.error_message) {
     toast.error(data.error_message);
@@ -29,7 +29,7 @@ export const getCode = async (address: string) => {
   }
 };
 export const reverseGeoCode = async (lat: number, lng: number) => {
-  const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
+  const URL = `${process.env.REACT_APP_CORS_PROXY}/https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
   const { data } = await axios(URL);
   if (data.error_message) {
     toast.error(data.error_message);
